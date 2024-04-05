b1fc4d1c04bed9c85310f9ddb924190f0301e00c
ronh <2039858744@qq.com>
2024-04-05 11:28:52 +0800

    :art: format

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -1,50 +1,50 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 // import { VirtualList } from './VirtualList';
-import VirtualList from '../dist';
+import VirtualList from "../dist";
 
 type itemType = {
-  id: number;
-  key: string;
+    id: number;
+    key: string;
 };
 
 const ListItem = (item: unknown, index: number) => {
-  return <div key={index}>{(item as itemType).key}</div>;
+    return <div key={index}>{(item as itemType).key}</div>;
 };
 
 function App() {
-  const [List, setList] = useState<unknown[]>([]);
+    const [List, setList] = useState<unknown[]>([]);
 
-  const handleAddMockData = () => {
-    const tmp = [...List];
-    for (let i = 0; i < 50; i++) {
-      tmp.push({
-        id: i,
-        key: `item ${i}`,
-      });
-    }
-    setList(tmp);
-  };
-  const handleClear = () => {
-    setList([]);
-  };
-  useEffect(() => {
-    handleAddMockData();
-  }, []);
-  return (
-    <>
-      <p className="read-the-docs">here is virtual list</p>
-      <button onClick={handleAddMockData}>mock data</button>
-      <button onClick={handleClear}>clear</button>
-      <VirtualList
-        data={List}
-        renderItem={ListItem}
-        itemHeight={50}
-        containerHeight={300}
-        buffer={2}
-      />
-    </>
-  );
+    const handleAddMockData = () => {
+        const tmp = [...List];
+        for (let i = 0; i < 50; i++) {
+            tmp.push({
+                id: i,
+                key: `item ${i}`,
+            });
+        }
+        setList(tmp);
+    };
+    const handleClear = () => {
+        setList([]);
+    };
+    useEffect(() => {
+        handleAddMockData();
+    }, []);
+    return (
+        <>
+            <p className="read-the-docs">here is virtual list</p>
+            <button onClick={handleAddMockData}>mock data</button>
+            <button onClick={handleClear}>clear</button>
+            <VirtualList
+                data={List}
+                renderItem={ListItem}
+                itemHeight={50}
+                containerHeight={300}
+                buffer={4}
+            />
+        </>
+    );
 }
 
 export default App;
--- code/src/VirtualList.tsx
+++ code/src/VirtualList.tsx
@@ -1,91 +1,94 @@
-import { useState, useRef, useEffect, useMemo, useCallback } from 'react';
-import type { VirtualListProps } from './VirtualList.d';
+import { useState, useRef, useEffect, useMemo, useCallback } from "react";
+import type { VirtualListProps } from "./VirtualList.d";
 
 export const VirtualList = (props: VirtualListProps<unknown>) => {
-  const { data, renderItem, itemHeight, containerHeight, buffer } = props;
+    const { data, renderItem, itemHeight, containerHeight, buffer } = props;
 
-  /**
-   * ? 内容总高度
-   */
-  const ViewHeight = useMemo(
-    () => data.length * itemHeight,
-    [data, itemHeight]
-  );
-  /**
-   * ? 缓存 可视区域 可容纳的数量
-   */
-  const Count = useMemo(
-    () => Math.ceil(containerHeight / itemHeight),
-    [containerHeight, itemHeight]
-  );
-  /**
-   * ? 合法 buffer 数量
-   */
-  const Buffer = useMemo(() => Math.ceil(buffer / 2), [buffer]);
-
-  const [startIndex, setStartIndex] = useState(0);
-  const [endIndex, setEndIndex] = useState(0);
-
-  const [PaddingTop, setPaddingTop] = useState(0);
-
-  const ref = useRef<HTMLDivElement>(null);
-  const isScrollBusy = useRef(false);
-
-  /**
-   * ? 处理数据，计算出 切片索引 & padding-top
-   */
-  const handleScroll = useCallback(() => {
-    if (isScrollBusy.current) {
-      return;
-    }
-    // open limit
-    isScrollBusy.current = true;
-
-    const { scrollTop } = ref.current as HTMLDivElement;
     /**
-     * ? 请求动画帧 避免画面抖动
+     * ? 内容总高度
      */
-    requestAnimationFrame(() => {
-      const start = Math.max(0, Math.floor(scrollTop / itemHeight) - Buffer);
-      const end = Math.min(data.length, start + Count + Buffer);
-      setStartIndex(start);
-      setEndIndex(end);
+    const ViewHeight = useMemo(
+        () => data.length * itemHeight,
+        [data, itemHeight]
+    );
+    /**
+     * ? 缓存 可视区域 可容纳的数量
+     */
+    const Count = useMemo(
+        () => Math.ceil(containerHeight / itemHeight),
+        [containerHeight, itemHeight]
+    );
+    /**
+     * ? 合法 buffer 数量
+     */
+    const Buffer = useMemo(() => Math.ceil(buffer / 2), [buffer]);
 
-      /**
-       * ? 用于占位，替代没有DOM的项，将已创建DOM的项 顶到可见区域
-       */
-      setPaddingTop(start * itemHeight);
+    const [startIndex, setStartIndex] = useState(0);
+    const [endIndex, setEndIndex] = useState(0);
 
-      // close limit
-      isScrollBusy.current = false;
-    });
-  }, [data, itemHeight, Count, Buffer]);
-  /**
-   * ? 初始化和props数据更新时
-   */
-  useEffect(() => {
-    handleScroll();
-  }, [handleScroll]);
+    const [PaddingTop, setPaddingTop] = useState(0);
 
-  return (
-    <div
-      style={{ overflow: 'auto', maxHeight: containerHeight }}
-      onScroll={handleScroll}
-      ref={ref as React.RefObject<HTMLDivElement>}
-    >
-      <section
-        style={{
-          height: ViewHeight,
-          paddingTop: PaddingTop,
-          boxSizing: 'border-box',
-        }}
-      >
-        {data.slice(startIndex, endIndex).map((item, index: number) => (
-          <div key={index} style={{ height: itemHeight }}>
-            {renderItem(item, index)}
-          </div>
-        ))}
-      </section>
-    </div>
-  );
+    const ref = useRef<HTMLDivElement>(null);
+    const isScrollBusy = useRef(false);
+
+    /**
+     * ? 处理数据，计算出 切片索引 & padding-top
+     */
+    const handleScroll = useCallback(() => {
+        if (isScrollBusy.current) {
+            return;
+        }
+        // open limit
+        isScrollBusy.current = true;
+
+        const { scrollTop } = ref.current as HTMLDivElement;
+        /**
+         * ? 请求动画帧 避免画面抖动
+         */
+        requestAnimationFrame(() => {
+            const start = Math.max(
+                0,
+                Math.floor(scrollTop / itemHeight) - Buffer
+            );
+            const end = Math.min(data.length, start + Count + Buffer);
+            setStartIndex(start);
+            setEndIndex(end);
+
+            /**
+             * ? 用于占位，替代没有DOM的项，将已创建DOM的项 顶到可见区域
+             */
+            setPaddingTop(start * itemHeight);
+
+            // close limit
+            isScrollBusy.current = false;
+        });
+    }, [data, itemHeight, Count, Buffer]);
+    /**
+     * ? 初始化和props数据更新时
+     */
+    useEffect(() => {
+        handleScroll();
+    }, [handleScroll]);
+
+    return (
+        <div
+            style={{ overflow: "auto", maxHeight: containerHeight }}
+            onScroll={handleScroll}
+            ref={ref as React.RefObject<HTMLDivElement>}
+        >
+            <section
+                style={{
+                    height: ViewHeight,
+                    paddingTop: PaddingTop,
+                    boxSizing: "border-box",
+                }}
+            >
+                {data.slice(startIndex, endIndex).map((item, index: number) => (
+                    <div key={index} style={{ height: itemHeight }}>
+                        {renderItem(item, index)}
+                    </div>
+                ))}
+            </section>
+        </div>
+    );
 };
